cadbe706a62dec79450f2cd92db03d2909fb6cf8
Bryan Jennings <bryanjenningz@gmail.com>
2016-09-09 20:45:26 -0700

    guest login works

--- client/index.js
+++ client/index.js
@@ -6,19 +6,36 @@ const rootEl = document.querySelector('#root')
 class App extends Component {
   constructor() {
     super()
-    guestLogin = this.guestLogin.bind(this)
+    this.guestLogin = this.guestLogin.bind(this)
+    this.login = this.login.bind(this)
+    this.state = {
+      username: '',
+      password: ''
+    }
   }
 
-  guestLogin() {
-    
+  login() {
+    console.log('Logged in!')
+  }
+
+  guestLogin(username, password) {
+    if (username.length === 0 && password.length === 0) {
+      this.login()
+    } else if (username.length === 0) {
+      this.setState({password: this.state.password + password[0]});
+      setTimeout(() => this.guestLogin(username, password.slice(1)), 50)
+    } else {
+      this.setState({username: this.state.username + username[0]});
+      setTimeout(() => this.guestLogin(username.slice(1), password), 50)
+    }
   }
 
   render() {
     return (
       <div>
-        <input />
-        <input type="password" />
-        <button onClick={guestLogin()}>Guest Login</button>
+        <input value={this.state.username} />
+        <input value={this.state.password} type="password" />
+        <button onClick={() => this.guestLogin('guest', 'password')}>Guest Login</button>
       </div>
     )
   }
